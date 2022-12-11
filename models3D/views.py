e50b20e98ed5e468a841fdb441bc51bff3ff7c38
Konstantin Kharitonov <hariton70@gmail.com>
2022-12-11 06:12:28 +0500

    11.12.2022

--- models3D/views.py
+++ models3D/views.py
@@ -7,11 +7,24 @@ def index(request):
     template = 'models3D/listmodels.html'
     page_number = int(request.GET.get('page', 1))
     content = os.listdir('files/')
-    paginator = Paginator(content, 10)
+    paginator = Paginator(content, 12)
     page_models = paginator.get_page(page_number)
+    list_models = []
+    for sample in page_models:
+        path_image = 'files/'+sample+'/images'
+        if os.path.isdir(path_image): 
+            images = os.listdir(path_image)
+            if len(images)>0:
+                list_models.append({'name':sample, 'image':'/'+path_image+'/'+images[0]})
+            else:
+                list_models.append({'name':sample, 'image':''})
+        else:
+            list_models.append({'name':sample, 'image':''})
+
     context = {
         'page_number': page_number,
         'page_models': page_models,
+        'list_models': list_models,
     }
     return render(request, template, context)
 
